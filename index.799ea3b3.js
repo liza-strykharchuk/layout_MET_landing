8972c895540ab479c67e51138089b62e7d17b0bd
Akilover <119785153+liza-strykharchuk@users.noreply.github.com>
2023-12-01 20:56:31 +0200

    make build

---
+++ index.799ea3b3.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--width-menu"):document.body.classList.remove("page__body--width-menu")}),document.querySelector("form").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.799ea3b3.js.map
